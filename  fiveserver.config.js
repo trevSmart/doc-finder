4e47608e8bcabf06a5c753c58b66736d1650505f
trevSmart <203040921+trevSmart@users.noreply.github.com>
2025-09-28 13:26:23 +0200

    next

---  fiveserver.config.js
+++
@@ -1,8 +0,0 @@
-// fiveserver.config.js
-module.exports = {
-  highlight: true, // enable highlight feature
-  injectBody: true, // enable instant update
-  remoteLogs: true, // enable remoteLogs
-  injectCss: false, // disable injecting css
-  navigate: true // enable auto-navigation
-};
--- src/LanguageContextData.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react'
-import type { Language } from './translations'
-
-export interface LanguageContextType {
-  language: Language
-  setLanguage: (language: Language) => void
-}
-
-export const LanguageContext = createContext<LanguageContextType | undefined>(undefined)
